28045f520b90f32900eb072a81d6abc818796376
Raghav Utpat <raghav1234.ru@gmail.com>
2018-12-23 21:37:39 +0530

    swapped lines nothing major

--- reply.py
+++ reply.py
@@ -20,6 +20,7 @@ text2=")    \n^(I'm a bot bleep-boop. I'm in beta, please don't hurt me. PM any 
 
 def reply(subr,num_comments):
 #TODO handle case where deletedcopy is not present
+    infile2='D:/Bot/'+subr.capitalize()+'/'+subr+'deleted.csv'
     infile='D:/Bot/'+subr.capitalize()+'/'+subr+'deletedcopy.csv'
     outfile='D:/Bot/'+subr.capitalize()+'/'+subr+'botcomments.csv'
     
@@ -27,8 +28,6 @@ def reply(subr,num_comments):
     df1.drop_duplicates(subset='comment_id',inplace=True)
     n=df1.count()
     random_rows=df1.sample(min(num_comments,n))['comment_id']
-    l=list(random_rows.index.values)
-    replied_to.append(l)
     for r_id in random_rows:
         comm=reddit.comment(r_id)
         comm.refresh()
@@ -37,11 +36,16 @@ def reply(subr,num_comments):
         returned_id=child_comment.reply(text1+r_id+text2)
         ret_id.append(returned_id)
         
+    '''Drop rows from deletedcopy using index values and write it to file'''
+    l=list(random_rows.index.values)
+    replied_to.append(l)
     df1=df1.drop(replied_to)
+    df1.to_csv(infile,index=False)
+
+
     ret_dict={'comment_id':ret_id}
     df3=pd.DataFrame.from_dict(ret_dict)
     
-    df1.to_csv(infile,index=False)
     if not os.path.isfile(outfile):
         df3.to_csv(outfile,index=False)
     else:
