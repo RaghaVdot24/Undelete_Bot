d4d2e5bfc14a159cf2fc961e30c11dafaafe8dc0
Raghav Utpat <raghav1234.ru@gmail.com>
2018-12-24 19:03:32 +0530

    Added undelete reply also fixed random replying
    Fixed problem where comment got deleted from db even when exception occurred

--- undel_reply.py
+++ undel_reply.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Dec 20 06:39:03 2018
+Created on Mon Dec 24 16:13:46 2018
 
 @author: Raghav Utpat
 """
@@ -12,13 +12,11 @@ import pandas as pd
 reddit=praw.Reddit('bot1')
 
 ret_id=[]
-text1="Hey anyone wants to see what was in the parent comment ?    \nAnyone wants to know the secret stuff ?    \n[Click here and I'll PM you](https://www.reddit.com/message/compose/?to=_undelete_Bot&subject=Undelete&message="
-text2=")    \n^(I'm a bot bleep-boop. I'm in beta, please don't hurt me. PM any problems [to this guy](https://reddit.com/user/crawlerx3001) or [this guy](https://reddit.com/user/VdotOne))    \n^(You can summon me by replying !undelete to a deleted comment(on select subs only)^)    \n^(Please visit r/undelete_Bot for more info)"
+text1="[Click here and I'll PM you the comment](https://www.reddit.com/message/compose/?to=_undelete_Bot&subject=Undelete&message="
+text2=")    \n^(I'm a bot bleep-boop. I'm in beta, please don't hurt me. PM any problems [to this guy](https://reddit.com/user/crawlerx3001) or [this guy](https://reddit.com/user/VdotOne))    \n^(You can summon me by replying with \"!undelete\" to the top child of a deleted comment(on select subs only)^)    \n^(Please visit r/undelete_Bot for more info)"
 
 
-
-def reply(subr,num_comments):
-#TODO handle case where deletedcopy is not present
+def undelete_reply(subr):
     infile='D:/Bot/'+subr.capitalize()+'/'+subr+'deletedcopy.csv'
     infile2='D:/Bot/'+subr.capitalize()+'/'+subr+'deleted.csv'
     outfile='D:/Bot/'+subr.capitalize()+'/'+subr+'botcomments.csv'
@@ -26,23 +24,17 @@ def reply(subr,num_comments):
     
     df1=pd.read_csv(infile)
     df1.drop_duplicates(subset='comment_id',inplace=True)
-    n=len(df1.index)
-    print(n)
-    x=min(num_comments,n)
-    random_rows=df1.sample(x)['comment_id']
-    for r_id in random_rows:
-        comm=reddit.comment(r_id)
-        comm.refresh()
-        child_comment=comm.replies[0]
-        print(child_comment)
-        try:
-            returned_id=child_comment.reply(text1+r_id+text2)
-        except praw.exceptions.APIException as e:
-            print(e.message)
+    for r_id in df1['comment_id']:
+        deleted_comm=reddit.comment(r_id)
+        deleted_comm.refresh()
+        child_comment=deleted_comm.replies[0]
+        (returned_id,flag)=reply_to_child(child_comment,r_id)
+        if(flag==False):
             continue
         replied_to.append(r_id)
-        ret_id.append(returned_id)
-        
+        ret_id.append(returned_id)        
+
+
     '''Drop rows from deletedcopy.csv using comment ids replied to and write it to file'''
     df1=df1[df1['comment_id']!=replied_to]
     df1.to_csv(infile,index=False)
@@ -64,12 +56,21 @@ def reply(subr,num_comments):
         df3.to_csv(outfile,mode='a',index=False,header=False)
 
 
+def reply_to_child(child_comment,r_id):
+    for child_reply in child_comment.replies:
+        if(child_reply.body=="!undelete" or child_reply.body=="!Undelete"):
+            try:
+                returned_id=child_reply.reply(text1+r_id+text2)
+                return(True,returned_id)
+            except praw.exceptions.APIException as e:
+                print(e.message)
+                return(False,0)
+
 def main():
     subs=['worldnews','news','todayilearned','politics','europe','tumblr']
-    num_comm=2
     for sub in subs:
         print('Running sub %s' %(sub))
-        reply(sub,num_comm)
+        undelete_reply(sub)
         
 if __name__=="__main__":
     main()
